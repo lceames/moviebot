2cfaa18dc367cf0749c9ba3d0ef9af800a749edb
Brian West <brian@signalwire.com>
2024-10-28 19:41:17 +0000

    remove some things

--- app.py
+++ app.py
@@ -553,8 +553,6 @@ def swaig_handler():
 
             new_url = urlunsplit((split_url.scheme, netloc, split_url.path, split_url.query, split_url.fragment))
             SWAIG_FUNCTION_SIGNATURES[func]["web_hook_url"] = f"{new_url}/swaig"
-            SWAIG_FUNCTION_SIGNATURES[func]["web_hook_auth_user"] = HTTP_USERNAME
-            SWAIG_FUNCTION_SIGNATURES[func]["web_hook_auth_pass"] = HTTP_PASSWORD
         
         if requested_functions == '':
             requested_functions = avaliable_functions
@@ -572,7 +570,10 @@ def swaig_handler():
 
         print(f"missing_functions: {missing_functions}")
 
-        return jsonify(response)  # Return the response with the requested function signatures
+        return app.response_class(
+            response=json.dumps(response, indent=None),
+            mimetype='application/json'
+        )
 
     else:
         function_name = data.get('function')
